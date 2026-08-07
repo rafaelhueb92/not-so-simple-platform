fd745d380385cc743be72f69ff8aa2d1a184305c
Rafael Hueb <rafaelhueb@192.168.1.4>
2026-08-07 01:01:36 -0300

    solve app-ci pipe

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,15 @@
-#module "eks" {
-#  source             = "git::https://github.com/rafaelhueb92/terraform-module-eks-poc.git//?ref=feat/taint-argocd"
-#  cluster_name       = var.cluster_name
-#  install_argocd     = true
-#  install_monitoring = true
-#  additional_admin_arns = [
-#    "arn:aws:iam::${local.account_id}:user/admin"
-#  ]
-#
-#  kubernetes_version = "1.35"
-#
-#}
+module "eks" {
+  source             = "git::https://github.com/rafaelhueb92/terraform-module-eks-poc.git//?ref=feat/taint-argocd"
+  cluster_name       = var.cluster_name
+  install_argocd     = true
+  install_monitoring = true
+  additional_admin_arns = [
+    "arn:aws:iam::${local.account_id}:user/admin"
+  ]
+
+  kubernetes_version = "1.35"
+
+}
 
 resource "aws_ecr_repository" "hello" {
   name = "hello-app"
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,10 +1,10 @@
-#output "cluster_name" {
-#  value = module.eks.cluster_name
-#}
-#
-#output "cluster_connection_commands" {
-#  value = module.eks.cluster_connection_commands
-#}
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_connection_commands" {
+  value = module.eks.cluster_connection_commands
+}
 
 output "ecr_registry_url" {
   value = aws_ecr_repository.hello.registry_id
